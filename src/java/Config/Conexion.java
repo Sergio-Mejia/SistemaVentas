bbf03de562bddadd61bc3d29347363a8a979a2ca
Sergio Mejia <dasergio27@gmail.com>
2021-10-06 22:35:56 -0500

    Conexion con la Base de Datos

--- src/java/Config/Conexion.java
+++ src/java/Config/Conexion.java
@@ -5,10 +5,29 @@
  */
 package Config;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author daser
  */
 public class Conexion {
+    Connection conexion;
     
+    private String URL = "jdbc:mysql://localhost:3306/sventas";
+    private String user = "root";
+    private String password = "";
+    
+    public Connection Conexion(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion = DriverManager.getConnection(URL, user, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Error SQL "+ ex.getMessage());
+        }  
+        return conexion;
+    }
 }
